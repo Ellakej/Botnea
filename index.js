e667d14bf01a655ae3b1350698433691e23a43c5
cisco1301 <32788069+cisco1301@users.noreply.github.com>
2020-06-07 11:27:02 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(bodyParser.json());
 
 // post request usando webhook
 app.post('/', function (req, res){
-
+console.log(req.body)
     // Query de las actions, si la action es "suma" entonces...
-    if (req.body.queryResult.action == "suma"){
+    if (req.body.queryResult.intent.displayName == "suma"){
         
         // Query de los numeros
         // Se necesita parsear a tipo flotante (talvez llega en tipo objeto)
@@ -31,10 +31,33 @@ app.post('/', function (req, res){
 
         // Output del bot
         response = num1 + " + " + num2 + " es " + sum;
+        console.log(response)
         res.json({
             "fulfillmentText": response
         });
     }
+
+if (req.body.queryResult.intent.displayName == "cantidad"){
+        
+        // Query de los numeros
+        // Se necesita parsear a tipo flotante (talvez llega en tipo objeto)
+        let num7 = parseFloat(req.body.queryResult.parameters.num7);
+        let num90 = parseFloat(15);
+        
+        // Suma
+        let suma1 = num7 * num90;
+
+        // Output del bot
+        response = "El total a pagar por tús productos es: " + suma1 + "\n¿Deseas agregar otro producto?";
+        console.log(response)
+        res.json({
+            "fulfillmentText": response
+        });
+    }
+
+
+
+
 });
 
 // Weas del servidor
